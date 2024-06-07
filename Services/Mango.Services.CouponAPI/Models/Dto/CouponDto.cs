e1f4d0b4756c2937af5a62f7a30569cb42bdf003
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-06-07 15:14:49 -0400

    fixed issues with intellisense add appdbcontext

---
+++ Services/Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double CouponAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
