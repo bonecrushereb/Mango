08b01fef1b7e74a6edf4f7cfffd1090a8b0605b1
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-06-14 16:37:44 -0400

    add automapper

--- Services/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Services/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -9,11 +11,14 @@ namespace Mango.Services.CouponAPI.Controllers
     {
         private readonly AppDbContext _db;
         private ResponseDto _response;
+        private IMapper _mapper;
 
-        public CouponAPIController(AppDbContext db)
+        public CouponAPIController(AppDbContext db, IMapper mapper)
         {
             _db = db;
+            _mapper = mapper;
             _response = new ResponseDto();
+
         }
 
         [HttpGet]
@@ -22,7 +27,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
-                _response.Result = objList;
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
             }
             catch (Exception ex)
             {
@@ -39,7 +44,24 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 Coupon obj = _db.Coupons.First(u=>u.CouponId==id);
-                _response.Result = obj;
+                _response.Result =  _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u=>u.CouponId==id);
+                _response.Result =  _mapper.Map<CouponDto>(obj);
             }
             catch (Exception ex)
             {
