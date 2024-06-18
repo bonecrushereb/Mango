474d539c760de76a3fcc4f022e37b11aa12446d9
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-06-18 10:42:32 -0400

    base service interface

---
+++ FrontEnd/Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+using static Mango.Web.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccesssToken { get; set; }
+    }
+}
---
+++ FrontEnd/Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,8 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web;
+
+public interface IBaseService
+{
+    Task<ResponseDto> SendAsync(RequestDto requestDto)
+}
