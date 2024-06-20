9e0a691a8a0d5c36857cd16b66389a01dff35b2b
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-06-20 10:19:44 -0400

    Base Service Implementation

---
+++ FrontEnd/Mango.Web/Services/IServices/IBaseServices.cs
@@ -0,0 +1,9 @@
+using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IBaseServices
+    {
+        Task<ResponseDto> SendAsync(RequestDto requestDto);
+    }
+}
