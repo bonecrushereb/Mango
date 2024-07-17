b78ac41d0b902990a9a820fb79d3f33e7d28ff03
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-07-17 15:03:45 -0400

    assign role

--- Services/Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Services/Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -41,5 +41,18 @@ namespace Mango.Services.AuthAPI.Controllers
             _response.Result = loginResponse;
             return Ok(_response);
         }
+
+        [HttpPost("AsignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+            var assignRoleSuccessful = await _authService.AssignRole(model.Email, model.Role.ToUpper());
+            if(!assignRoleSuccessful)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Error Encountered";
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
     }
 }
--- Services/Mango.Services.AuthAPI/Service/AuthService.cs
+++ Services/Mango.Services.AuthAPI/Service/AuthService.cs
@@ -20,6 +20,22 @@ namespace Mango.Services.AuthAPI.Service
            _roleManger = roleManager;
         }
 
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if(user != null)
+            {
+                if(!_roleManger.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    //create role if it does not exist
+                    _roleManger.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             var user = _db.ApplicationUsers.FirstOrDefault(u=>u.UserName.ToLower()==loginRequestDto.UserName.ToLower());
