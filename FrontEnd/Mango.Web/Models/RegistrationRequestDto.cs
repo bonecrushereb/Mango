935f0ab1d6a7f7f70e0ab1ef21621cd55299a8c6
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-07-18 10:50:28 -0400

    add Auth service to front end

---
+++ FrontEnd/Mango.Web/Models/RegistrationRequestDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Models
+{
+    public class RegistrationRequestDto
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
