a6b6eeeec162055d0f8ff551a78f8f37e5c26add
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-08-08 12:48:19 -0400

    project working

--- Services/Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ Services/Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -1,6 +1,9 @@
-using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Mango.Web.Models
+namespace Mango.Services.ProductAPI.Models.Dto
 {
     public class ProductDto
     {
@@ -10,7 +13,5 @@ namespace Mango.Web.Models
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
-        [Range(1,100)]
-        public int Count { get; set; }  = 1;
     }
 }
--- Services/Mango.Services.ProductAPI/Models/Product.cs
+++ Services/Mango.Services.ProductAPI/Models/Product.cs
@@ -1,16 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Mango.Web.Models
+namespace Mango.Services.ProductAPI.Models
 {
-    public class ProductDto
+    public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
-        public string ImageUrl { get; set; }
-        [Range(1,100)]
-        public int Count { get; set; }  = 1;
+        public string? ImageUrl { get; set; }
+        public string? ImageLocalPath { get; set; }
     }
 }
