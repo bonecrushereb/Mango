9a0f1c16bf839a45b48cd865c5e4eed3ffaea500
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-08-20 13:29:09 -0400

    add shopping cart to app

--- Services/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
+++ Services/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -10,7 +10,7 @@ namespace Mango.Services.ShoppingCartAPI.Models
         public int CartDetailsId { get; set; }
         public int CartHeaderId { get; set; }
         [ForeignKey("CartHeaderId")]
-        public CartHeaderDto CartHeader { get; set; }
+        public CartHeader CartHeader { get; set; }
         public int ProductId { get; set; }
         [NotMapped]
         public ProductDto Product { get; set; }
--- Services/Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
+++ Services/Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -4,7 +4,7 @@ namespace Mango.Services.ShoppingCartAPI.Models.Dto
     {
         public int CartDetailsId { get; set; }
         public int CartHeaderId { get; set; }
-        public CartHeaderDto CartHeader { get; set; }
+        public CartHeader CartHeader { get; set; }
         public int ProductId { get; set; }
         public ProductDto Product { get; set; }
         public int Count { get; set; }
